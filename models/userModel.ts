92c9fb4184ac7654635029643129d96a8c55108d
Cibiyanna <pkcibiyanna@gmail.com>
2024-02-24 11:29:17 +0530

    added the user model

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,1 +1,30 @@
-import mongoose  from "mongoose"; 
+import mongoose, { Document } from 'mongoose';
+
+interface UserDocument extends Document {
+  username: string;
+  age: number;
+  email: string;
+  role:string;
+  educationEndYear: Date | null;
+  departmentOfStudy: string | null;
+  committeeBelonging: string | null;
+  password: string;
+}
+
+const UserSchema = new mongoose.Schema<UserDocument>(
+  {
+    username: { type: String, required: true },
+    age: Number,
+    email: { type: String, required: true, unique: true },
+    role:String,
+    educationEndYear: Date,
+    departmentOfStudy: String,
+    committeeBelonging: String,
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model<UserDocument>('User', UserSchema);
+
+export default UserModel;
