7d31bf1288bc232e55987833b7f9f7216f333c27
Cibiyanna <pkcibiyanna@gmail.com>
2024-02-24 19:48:43 +0530

    middleware added

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -10,11 +10,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   switch (method) {
     case "GET":
       await connectDB();
-      await authenticateMiddleware(req , res, async () => {
-        return res.status(200).json({ message: 'Secure route accessed' });
-      });
+      
+      // await authenticateMiddleware(req , res, async () => {
+      //   return res.status(200).json({ message: 'Secure route accessed' });
+      // });
 
-    
     default:
       return res.status(405).end(`Method ${method} Not Allowed`);
   }
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,14 +1,17 @@
 import { connectDB } from '@/models/db';
 import type { NextApiRequest, NextApiResponse } from 'next';
-
+import { authenticateMiddleware,userDetailsFromToken , UserPayload } from '@/service/user.service';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Your logic here
   const method = req.method;
   
   switch (method) {
     case "GET":
-      await connectDB();
-      return res.status(200).json({ name: "John Doe" });
+      await authenticateMiddleware(req,res,()=>{
+        const token = req.cookies.token as string;
+        const tokenDetials = userDetailsFromToken(token) as UserPayload;
+        return res.status(200).json({error:false,tokenDetials,username:tokenDetials.username})
+      })
     
     default:
       return res.status(405).end(`Method ${method} Not Allowed`);
