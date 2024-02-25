bd87342c77f6b1f7d02c31e96f2e3c249afa5f1f
Cibiyanna <pkcibiyanna@gmail.com>
2024-02-25 19:22:54 +0530

    h

--- app/(default)/dashboard/[hallid]/page.tsx
+++ app/(default)/dashboard/[hallid]/page.tsx
@@ -61,7 +61,7 @@ const Page = ({params}:{params:{hallid:string}}) => {
  
     async function actDateBooks(year : number, month : number, day : number){
         try{
-            const response = await axios.get(`${HOSTED_URL}/api/room-management/helper.booking`);
+            const response = await axios.get(`${HOSTED_URL}/api/room-management/helperbooking`);
             const data = response.data.message;
             var newMonth ;
             if(month<10)newMonth = "0"+month;
